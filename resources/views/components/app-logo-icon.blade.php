30345ad2b2698f33e9f61ccea343d42438238afe
Adonyo W. Emmanuel <amazing.chief23@gmail.com>
2026-06-23 14:18:24 +0300

    Polish sidebar logo: SVG badge with business initials on rounded rect

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -5,4 +5,7 @@
         ? strtoupper(substr($words[0], 0, 1) . substr(end($words), 0, 1))
         : strtoupper(substr($name, 0, 2));
 @endphp
-<span {{ $attributes->merge(['class' => 'flex items-center justify-center text-[10px] font-bold leading-none']) }}>{{ $initials }}</span>
+<svg {{ $attributes->merge(['viewBox' => '0 0 40 40', 'xmlns' => 'http://www.w3.org/2000/svg', 'class' => 'size-5']) }}>
+    <rect width="40" height="40" rx="10" ry="10" fill="currentColor" />
+    <text x="20" y="20" text-anchor="middle" dominant-baseline="central" font-family="system-ui,-apple-system,sans-serif" font-weight="700" font-size="18" letter-spacing="1" fill="rgb(0,0,0)" class="dark:fill-white">{{ $initials }}</text>
+</svg>
